3ea0c6fe700f1051167373bb717e75bc09bfd5fd
oscartrugo <oscartrugo@gmail.com>
2022-04-08 02:35:24 -0500

    Watch mode

--- bases/app.js
+++ bases/app.js
@@ -1,2 +1,10 @@
-var msg = 'Hola mundo'; //Constante 'msg' de tipo String 
-console.log(msg);
+"use strict";
+const msg = 'Hola mundo'; //Constante 'msg' de tipo String 
+const heroe = {
+    name: 'Ironman',
+    age: 45
+};
+heroe.age = 50;
+console.log(heroe.age + 1);
+//ejecutamos en consola "tsc app" para traducir el código de TS a JS
+//Con el comando tsc --init creamos el archivo tsconfig.json
--- bases/app.ts
+++ bases/app.ts
@@ -1,6 +1,14 @@
 
 const msg: string = 'Hola mundo' //Constante 'msg' de tipo String 
 
-console.log(msg)
+const heroe = {
+    name: 'Ironman',
+    age: 45
+}
+
+heroe.age = 50
+
+console.log(heroe.age + 1)
 
 //ejecutamos en consola "tsc app" para traducir el código de TS a JS
+//Con el comando tsc --init creamos el archivo tsconfig.json
