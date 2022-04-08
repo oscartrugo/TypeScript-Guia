0c76b6850b5e573b1507d0c65ffcef938fdc4b4f
oscartrugo <oscartrugo@gmail.com>
2022-04-08 02:21:45 -0500

    Hola mundo en TS

---
+++ bases/app.js
@@ -0,0 +1,2 @@
+var msg = 'Hola mundo'; //Constante 'msg' de tipo String 
+console.log(msg);
---
+++ bases/app.ts
@@ -0,0 +1,6 @@
+
+const msg: string = 'Hola mundo' //Constante 'msg' de tipo String 
+
+console.log(msg)
+
+//ejecutamos en consola "tsc app" para traducir el código de TS a JS
