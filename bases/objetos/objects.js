98a540f784b64d94ce5ac0a29f1c925a51bd8fbc
oscartrugo <oscartrugo@gmail.com>
2022-04-11 01:14:22 -0500

    35. Objetos basicos

---
+++ bases/objetos/objects.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'viajar en el tiempo']
+    };
+    flash = {
+        name: 'Clark Kent',
+        age: 24,
+        powers: ['Super fuerza'],
+        getNombre() {
+            return this.name;
+        }
+    };
+});
---
+++ bases/objetos/objects.ts
@@ -0,0 +1,16 @@
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'viajar en el tiempo']
+    }
+
+    flash = {
+        name: 'Clark Kent',
+        age: 24,
+        powers: ['Super fuerza']
+        getNombre(){
+            return this.name;
+        }
+    }
+})
