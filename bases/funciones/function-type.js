59d70e7073e79fd4ad8c4a42a5058c018e512c01
oscartrugo <oscartrugo@gmail.com>
2022-04-10 19:27:23 -0500

    32. Tipo Función

---
+++ bases/funciones/function-type.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    const addNumbers = (a, b) => a + b;
+    const greet = (name) => `Hola ${name}`;
+    const saveTheWorld = () => 'El mundo está salvado!';
+    // let myFunction: Function; //Solo acepta funciones
+    let myFunction; //Solo acepta funciones
+    // myFunction = 10
+    // console.log(myFunction)
+    // myFunction = addNumbers //Esta función se la asignamos a myFunction
+    // console.log(myFunction(1, 2)) //TS infiere que los argumentos son numbers
+    // myFunction = greet
+    // console.log('Oscar')
+    myFunction = saveTheWorld;
+    console.log(myFunction()); //TypeScript infiere los tipos
+})();
