3a8478d7a34624da9e939f6f76bdb2839b82a2c2
oscartrugo <oscartrugo@gmail.com>
2022-04-13 22:18:14 -0500

    59. Forma corta de asignar propiedades

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -1,20 +1,22 @@
 (() => {
 
     class Avenger {
-        private name: string; //Sólo tenemos acceso a esta propiedad dentro de la clase
-        private team: string;
-        public realName?: string;
-        static avgAge: number = 35; //static 
+        //private name: string = 'Scott Lang'; //Sólo tenemos acceso a esta propiedad dentro de la clase
+        //private team: string;
+        //public realName?: string;
+        static avgAge: number = 35; //No podemos mandar static prop como argumento al constructor 
 
-        constructor(name: string, team: string, realName: string){
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
-
+        constructor( 
+            private name: string, 
+            private team: string, 
+            public realName?: string,
+            avgAge2: number = 55,
+            ){
+                //Avenger.avgAge = avgAge2
+            }
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Ron');
+    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman)
     console.log(Avenger.avgAge) //Las props estáticas se pueden consumir de manera global haciendo referencia a la clase
 
--- bases/main.js
+++ bases/main.js
@@ -1,14 +1,14 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
+        constructor(name, team, realName, avgAge2 = 55) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
     }
     Avenger.avgAge = 35;
-    const antman = new Avenger('Antman', 'Capitan', 'Ron');
+    const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
     console.log(Avenger.avgAge);
 })();
