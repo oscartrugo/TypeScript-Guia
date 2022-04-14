d60056c2296b4f18160e07e2b04e32475073abcb
oscartrugo <oscartrugo@gmail.com>
2022-04-13 22:28:05 -0500

    60. Métodos públicos y privados

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -5,21 +5,26 @@
         //private team: string;
         //public realName?: string;
         static avgAge: number = 35; //No podemos mandar static prop como argumento al constructor 
+        static getavgAge(){
+            return this.name
+        } //Método estático
 
         constructor( 
             private name: string, 
             private team: string, 
             public realName?: string,
             avgAge2: number = 55,
-            ){
-                //Avenger.avgAge = avgAge2
+            ){}
+
+            public bio(){
+                return `${ this.name } (${ this.team })`
             }
     }
 
     const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman)
     console.log(Avenger.avgAge) //Las props estáticas se pueden consumir de manera global haciendo referencia a la clase
-
+    console.log(Avenger.getavgAge())
 
 
 })()
--- bases/main.js
+++ bases/main.js
@@ -6,10 +6,17 @@
             this.team = team;
             this.realName = realName;
         }
+        static getavgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
     console.log(Avenger.avgAge);
+    console.log(Avenger.getavgAge());
 })();
 //# sourceMappingURL=main.js.map
