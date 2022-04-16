1716219b5fa7379f027567d050a712270c8d6c21
oscartrugo <oscartrugo@gmail.com>
2022-04-16 15:45:22 -0500

    78. Imports y Exports

---
+++ ts-node/src/classes/Hero.ts
@@ -0,0 +1,7 @@
+export class Hero { //Exportamos la clase Hero
+    constructor(
+        public name: string,
+        public powerId: number,
+        public age: number,
+    ){}
+}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,6 +1,10 @@
-
+import { Hero } from './classes/Hero';
 
 console.log('Hola Mundo!!!');
+
+const ironman:Hero = new Hero('Ironman', 500, 45);
+
+console.log(ironman)
 
 
 
