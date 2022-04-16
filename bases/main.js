17a25cd9b5d7e88301f21a953d149072892bbb37
oscartrugo <oscartrugo@gmail.com>
2022-04-16 13:28:33 -0500

    76. Creando un Namespace

--- bases/main.js
+++ bases/main.js
@@ -1,63 +1,10 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Super velocidad', 'viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => (text.length > 3) ? true : false;
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Super velocidad']
-    };
-    let arrow = {
-        name: 'Oliver Queen',
-        age: 32,
-        powers: ['Artes marciales', 'Arqueria', 'Liderazgo']
-    };
-})();
-(() => {
-    class Mutant {
-        constructor() {
-            this.age = 4;
-            this.name = 'oscartrugo';
-            this.realName = 'Oscar';
-        }
-        mutantPower(id) {
-            return this.name + '' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Oscar',
-        age: 24,
-        address: {
-            id: 125,
-            zip: 'KDCRNW',
-            city: 'New York'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Diana',
-        age: 23,
-        address: {
-            id: 234,
-            zip: 'ola',
-            city: 'Toronto'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return b + a;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Oscar'));
 //# sourceMappingURL=main.js.map
